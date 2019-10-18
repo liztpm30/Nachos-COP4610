f8aab39089f50408cee29b4e5eab76d0750a7e46
0322wendy <worel001@fiu.edu>
2019-10-18 19:36:03 -0400

    Update addrspace.h
    Added translate function

--- code/userprog/addrspace.h
+++ code/userprog/addrspace.h
@@ -30,6 +30,8 @@ class AddrSpace {
 
     void SaveState();			// Save/restore address space-specific
     void RestoreState();		// info on a context switch 
+	
+    int Translate(int virtAddr);	// Converts virtAddr to physAddr
 
   private:
     TranslationEntry *pageTable;	// Assume linear page table translation
