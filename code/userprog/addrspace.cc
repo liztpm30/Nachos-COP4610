854d6d3c98766d669d989ea875a41966d0094f42
0322wendy <worel001@fiu.edu>
2019-10-18 19:34:32 -0400

    Update addrspace.cc

--- code/userprog/addrspace.cc
+++ code/userprog/addrspace.cc
@@ -184,3 +184,56 @@ void AddrSpace::RestoreState()
     machine->pageTable = pageTable;
     machine->pageTableSize = numPages;
 }
+
+//----------------------------------------------------------------------
+// AddrSpace::Translate
+//      Converts a virtual address to a physical address. It uses
+//      a page table. First, it checks for alignment errors and then
+//      if everything is ok, it sets the dirty bits in the table
+//      entry and stores the translated physical address in 'physAddr'.
+//      Returns exception if there's an error.
+//
+//      "virtAddr" - the virtual address to translate
+//      "physAddr" - the place to store teh physical address
+//      "size" - the amount of memory being read or written
+//----------------------------------------------------------------------
+
+int AddrSpace::Translate(int virtAddr)
+{
+    // Create a physical address
+    int size;
+    int physAddr;
+    bool writing;
+    int vpn = (unsigned) virtAddr / PageSize;
+    int offset = (unsigned) virtAddr % PageSize;
+    TranslationEntry *entry;
+    unsigned int pageFrame;
+
+    DEBUG('a', "\tTranslate 0x%x, %s: ", virtAddr, writing ?
+                "write" : "read");
+
+    // Calculate the page number and offset within the page from
+    // the virtual address
+    if (vpn >= numPages) {
+        DEBUG('a', "Virtual page # %d too large for numPages %d!\n",
+            virtAddr, numPages);
+        return -1;
+    }
+    if (!pageTable[vpn].valid) {
+        DEBUG('a', "Virtual page # %d is not valid!\n",
+            virtAddr);
+        return -1;
+    }
+    entry = &pageTable[vpn];
+
+    pageFrame = entry->physicalPage;
+
+    // set the use, dirty bits
+    entry->use = TRUE;
+    if (writing)
+        entry->dirty = TRUE;
+    physAddr = pageFrame * PageSize + offset;
+    DEBUG('a', "translate vpn %d to pfn %d\n", virtAddr, physAddr);
+
+    return physAddr;
+}
